7746639c28895ffaecd1a76e12296c4319fd0906
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-06-20 17:42:14 -0500

    test

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from messages.message_helper import get_text_message_input, send_message
 
 app = Flask(__name__)
 
-with open('config.json') as f:
-    config = json.load(f)
+# with open('config.json') as f:
+#     config = json.load(f)
 
-app.config.update(config)
+# app.config.update(config)
 
 
 @app.route("/")
@@ -16,12 +16,12 @@ def index():
     return render_template('index.html', name=__name__)
 
 
-@app.route('/welcome', methods=['POST'])
-async def welcome():
-    data = get_text_message_input(
-        app.config['RECIPIENT_WAID'], 'Welcome to the Flight Confirmation Demo App for Python!')
-    await send_message(data)
-    return flask.redirect(flask.url_for('index'))
+# @app.route('/welcome', methods=['POST'])
+# async def welcome():
+#     data = get_text_message_input(
+#         app.config['RECIPIENT_WAID'], 'Welcome to the Flight Confirmation Demo App for Python!')
+#     await send_message(data)
+#     return flask.redirect(flask.url_for('index'))
 
 
 if __name__ == "__main__":

3c6708147a8d36870d38feed773f3b8d3b722318
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-07-03 15:11:34 -0500

    testing interactive message

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 from flask import Flask, render_template, request, jsonify
 import flask
-from messages.message_helper import get_text_message_input, send_message
+from messages.message_helper import get_text_message_input, send_message, is_message_valid
 from dotenv import load_dotenv
 import os
 import requests
@@ -27,67 +27,21 @@ app.config.update(config)
 async def webhook_whatsapp():
     """__summary__: Get message from the webhook"""
 
-    print("sakdfmaskdmfaskd", request)
     if request.method == "GET":
         if request.args.get('hub.verify_token') == config["VERIFY_TOKEN"]:
             return request.args.get('hub.challenge')
         return "Authentication failed. Invalid Token."
 
-    body = request.json
+    data = request.json
 
-    # Check the Incoming webhook message
-    # app.logger.info(json.dumps(body, indent=2))
+    if not is_message_valid(data):
+        return "Invalid message."
 
-    # app.logger.info(body['entry'][0]['changes'][0]
-    #                 ['value']['messages'][0]['text']['body'])
-
-    if 'object' in body:
-        if (
-            'entry' in body and
-            body['entry'][0].get('changes') and
-            body['entry'][0]['changes'][0].get('value') and
-            body['entry'][0]['changes'][0]['value'].get('messages') and
-            body['entry'][0]['changes'][0]
-            ['value']['messages'][0]['text']['body'] == "agendar"
-        ):
-            phone_number_id = body['entry'][0]['changes'][0]['value']['metadata']['phone_number_id']
-            from_number = body['entry'][0]['changes'][0]['value']['messages'][0]['from']
-            msg_body = body['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
-
-            app.logger.info(msg_body)
-
-            data = get_text_message_input(
-                app.config['RECIPIENT_WAID'], 'Welcome to the Flight Confirmation Demo App for Python!')
-            await send_message(data)
-
-            # Make a POST request to send a message back
-            # url = f"https://graph.facebook.com/v12.0/{phone_number_id}/messages?access_token={config['VERIFY_TOKEN']}"
-            # data = {
-            #     'messaging_product': 'whatsapp',
-            #     'to': from_number,
-            #     'text': {'body': f"Ack: {msg_body}"}
-            # }
-            # headers = {'Content-Type': 'application/json'}
-            # response = requests.post(url, json=data, headers=headers)
-            # if response.status_code == 200:
-            #     print("Message sent successfully")
+    message = get_text_message_input(
+        app.config['RECIPIENT_WAID'], 'Welcome to the Flight Confirmation Demo App for Python!')
+    await send_message(message)
 
     return jsonify({'status': 'success'}), 200
-
-    # RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
-    # EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
-    # mensaje = "Telefono:" + \
-    #     data['entry'][0]['changes'][0]['value']['messages'][0]['from']
-    # mensaje = mensaje+"|Mensaje:" + \
-    #     data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
-    # ESCRIBIMOS EL NUMERO DE TELEFONO Y EL MENSAJE EN EL ARCHIVO TEXTO
-    # f = open("texto.txt", "w")
-    # f.write(data)
-    # f.close()
-    # print(data)
-    # print(request)
-    # print("something")
-    # RETORNAMOS EL STATUS EN UN JSON
 
 
 @app.route("/")
--- messages/message_helper.py
+++ messages/message_helper.py
@@ -3,6 +3,28 @@ import json
 from flask import current_app
 
 
+def is_message_valid(data: dict):
+    if 'object' not in data:
+        return False
+
+    if 'entry' not in data:
+        return False
+
+    if not data['entry'][0].get('changes'):
+        return False
+
+    if not data['entry'][0]['changes'][0].get('value'):
+        return False
+
+    if not data['entry'][0]['changes'][0]['value'].get('messages'):
+        return False
+
+    if not data['entry'][0]['changes'][0]['value']['messages'][0]:
+        return False
+
+    return True
+
+
 async def send_message(data):
     headers = {
         "Content-type": "application/json",
@@ -28,16 +50,72 @@ async def send_message(data):
 
 
 def get_text_message_input(recipient, text):
-    return json.dumps({
-        "messaging_product": "whatsapp",
-        "preview_url": False,
-        "recipient_type": "individual",
-        "to": recipient,
-        "type": "text",
-        "text": {
-            "body": text
+    # return json.dumps({
+    #     "messaging_product": "whatsapp",
+    #     "preview_url": False,
+    #     "recipient_type": "individual",
+    #     "to": recipient,
+    #     "type": "text",
+    #     "text": {
+    #         "body": text
+    #     }
+    # })
+
+    return json.dumps(
+
+
+        {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": recipient,
+            "type": "interactive",
+            "interactive": {
+                "type": "list",
+                "header": {
+                    "type": "text",
+                    "text": "HEADER_TEXT"
+                },
+                "body": {
+                    "text": "BODY_TEXT"
+                },
+                "footer": {
+                    "text": "FOOTER_TEXT"
+                },
+                "action": {
+                    "button": "BUTTON_TEXT",
+                    "sections": [
+                        {
+                            "title": "SECTION_1_TITLE",
+                            "rows": [
+                                {
+                                    "id": "SECTION_1_ROW_1_ID",
+                                    "title": "SECTION_1_ROW_1_TITLE",
+                                    "description": "SECTION_1_ROW_1_DESCRIPTION"
+                                },
+                                {
+                                    "id": "SECTION_1_ROW_2_ID",
+                                    "title": "SECTION_1_ROW_2_TITLE",
+                                    "description": "SECTION_1_ROW_2_DESCRIPTION"
+                                }
+                            ]
+                        },
+                        {
+                            "title": "SECTION_2_TITLE",
+                            "rows": [
+                                {
+                                    "id": "SECTION_2_ROW_1_ID",
+                                    "title": "SECTION_2_ROW_1_TITLE",
+                                    "description": "SECTION_2_ROW_1_DESCRIPTION"
+                                },
+                                {
+                                    "id": "SECTION_2_ROW_2_ID",
+                                    "title": "SECTION_2_ROW_2_TITLE",
+                                    "description": "SECTION_2_ROW_2_DESCRIPTION"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
         }
-    })
-
-
-1
+    )

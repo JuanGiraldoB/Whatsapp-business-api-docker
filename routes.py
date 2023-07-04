56e9f5d6798c9569c66a807cb5be017e35c68a0a
JuanGiraldoB <jugiraldob@unal.edu.co>
2023-07-04 09:04:09 -0500

    t.

--- routes.py
+++ routes.py
@@ -18,18 +18,15 @@ async def webhook_whatsapp():
         return "Authentication failed. Invalid Token."
 
     data = request.json
-#
+
     if not exists_message(data):
         message = get_text_message_input(
             current_app.config['RECIPIENT_WAID'], False)
         await send_message(message)
 
-    msg = get_message(data)
-
-    if is_valid_message(msg):
-        message = get_text_message_input(
-            current_app.config['RECIPIENT_WAID'], True)
-        await send_message(message)
+    message = get_text_message_input(
+        current_app.config['RECIPIENT_WAID'], True)
+    await send_message(message)
 
     return jsonify({'status': 'success'}), 200
 
